f7acec1b2b95166469a7af1054bd58830a41d877
DIOGO SOARES <diogo@i9me.com.br>
2018-07-28 10:11:03 -0300

    Intro

--- template-parts/page/intro-page.php
+++ template-parts/page/intro-page.php
@@ -1,13 +1,13 @@
 <?php if( is_home() || is_front_page() ) { ?>
-<section id="up" class="Section--intro Intro Intro--home Section--style1 u-positionRelative u-flex u-flexDirectionColumn u-flexAlignItemsCenter u-flexJustifyContentCenter u-paddingVertical" data-paroller-factor="-0.20" data-paroller-factor-xs="0.2">
+<section id="up" class="Section--intro Intro Intro--home Section--style1 u-positionRelative u-flex u-flexDirectionColumn u-flexAlignItemsFlexStart u-flexJustifyContentCenter u-flexAlignItemsFlexStart u-paddingVertical" data-paroller-factor="-0.20" data-paroller-factor-xs="0.2">
 
 
-	<div class="Captions u-displayFlex u-flexJustifyContentCenter">		
-		<h2 class="Captions--title">
-			<span class="u-displayBlock">Legenda</span>
-			
-			
-		</h2>
+	<div class="Captions u-positionRelative u-displayFlex u-flexJustifyContentCenter u-size9of24 u-flexDirectionColumn u-flexAlignItemsCenter">	
+	<figure class="ArabescoTop--intro"></figure>
+		<h1 class="Captions--title color_3">
+			<span class="u-displayBlock U-sizeFull">Empreenda junto com a</span>			
+		</h1>
+	<figure class="ArabescoBottom--bottom u-marginTop--inter--half"></figure>
 		
 	</div>
 
